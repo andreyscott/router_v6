dabf871500f1b17352ec0e458871a42046f566fc
andreyscott <andreyscott301@gmail.com>
2022-04-08 17:46:26 +0100

    pratice makes champions

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import { Routes } from "react-router-dom"
+import { Route, useNavigate } from "react-router-dom"
+
+// nested routes
+import Offers from "./Offers"
+
+export default function About() {
+  const nav = useNavigate()
+
+  return (
+    <div className="content">
+      <h2>About Us</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
+      
+      <p>Lorem ipsum dolor, sit ament consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
+ 
+ <button onClick={() => nav("/products")}>Go to Products
+ </button>
+
+ <Routes>
+    <Route path="/offers" element={<Offers />} />
+ </Routes>
+
+    
+    </div>
+  )
+}
